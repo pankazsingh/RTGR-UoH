8bc07335e327f7417fb0ea992ed7ba3a4c072ebe
pankazsingh <pankaz.d@gmail.com>
2020-05-27 17:37:20 +0530

    Update fetchGeneSeq.py

--- fetchGeneSeq.py
+++ fetchGeneSeq.py
@@ -1,3 +1,17 @@
 # This file used to fetch teh gene sequence from the genome file 
-# based of the gene coordinates provided as command line arguments.
+# based of the gene coordinates (start and end position) provided as command line arguments.
 #
+import sys
+filename = sys.argv[3]+".fa" # path to the genome file
+f = open(filename, "r")
+seq = f.read()
+
+start = int(sys.argv[1])
+end = int(sys.argv[2])
+
+
+sequence = seq[start:end]
+print(sequence)
+print(sys.argv[3])
+
+#print(sys.argv[2])
